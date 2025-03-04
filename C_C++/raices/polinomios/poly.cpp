07dbcb125da8388f1853694bbe9bddb07b8c51e0
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2025-03-04 11:01:32 -0500

    Ampliado el ejemplo de C/C++ de polinomios con los argumentos del main.

--- C_C++/raices/polinomios/poly.cpp
+++ C_C++/raices/polinomios/poly.cpp
@@ -3,9 +3,22 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+  if(argc == 1){
+    cout << "Debe ingresar los coeficientes a0 a1 a2 ... del respectivo polinomio.\n";
+    return -1;
+  }
 
-  Eigen::Vector3d coeff(-0.2151830138973625, -0.3111717537041549, 0.708563939215852);
+  Eigen::VectorXd coeff( argc - 1);
+
+  double tmp;
+
+  for(int i = 0; i < argc - 1; i++){
+    tmp = atof( argv[i+1] );
+    coeff(i) = tmp;
+  }
+
+  
   Eigen::PolynomialSolver<double, Eigen::Dynamic> solver;
   solver.compute(coeff);
   const Eigen::PolynomialSolver<double, Eigen::Dynamic>::RootsType &r = solver.roots();
